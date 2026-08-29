2a0552294ecbdb1aa054dccd2d3762c48e8247c9
{sataporn.sup} <{sataporn.sup@gmail.com}>
2026-08-29 14:30:16 +0700

    feat: rename photo-booth to TikTok filter and play clip in center

--- src/app/features/photo-booth/photo-booth.component.ts
+++ src/app/features/photo-booth/photo-booth.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { StickyNoteButtonComponent } from '../../shared/components/sticky-note-button/sticky-note-button.component';
 
 @Component({
   selector: 'app-photo-booth',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, StickyNoteButtonComponent, RouterLink],
   templateUrl: './photo-booth.component.html',
   styleUrl: './photo-booth.component.scss',
 })
